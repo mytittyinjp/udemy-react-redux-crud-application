28af3c41adfb6754e9b589ca194819b4cb46bb48
n_horie <mytittyinjp@gmail.com>
2020-08-13 21:09:18 +0900

    create class compornent and fuctional compornent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+// import React from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={()=>{console.log("I'm clicked")}} />
+//     </React.Fragment>
+//   );
+// }
+
+
+
+// class compornent
+// import React, { Component } from "react";
+
+// class App extends Component {
+//   render(){
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={()=>{console.log("I'm clicked")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+import React from "react";
+
+// function compornent
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={()=>{console.log("I'm clicked")}} />
-    </React.Fragment>
-  );
+    <div>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
